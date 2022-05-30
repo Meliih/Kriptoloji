3ef784eca18539b2f770de702b7221ef9212a243
Meliih <melihkahraman4@gmail.com>
2022-05-30 17:24:24 +0300

    final project level 3

--- FinalProject/README.md
+++ FinalProject/README.md
@@ -17,3 +17,25 @@
   &nbsp;&nbsp;&nbsp;&nbsp;
   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/metamask_transfer.PNG" alt="drawing" width="230"/>
 </p>
+
+<p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-1.PNG" alt="drawing" width="230"/>
+</p>
+<p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-2.PNG" alt="drawing" width="230"/>
+</p>
+<p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-3.PNG" alt="drawing" width="230"/>
+</p>
+<p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-4.PNG" alt="drawing" width="230"/>
+</p>
+<p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-5.PNG" alt="drawing" width="230"/>
+</p>
+<p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-6.PNG" alt="drawing" width="230"/>
+</p>
+ <p>
+   <img src="https://github.com/Meliih/Kriptoloji/blob/main/FinalProject/github_image/3-7.PNG" alt="drawing" width="230"/>
+</p>
---
+++ FinalProject/contracts/DeferredEquityPlan.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.5.0;
+
+// lvl 3: equity plan
+contract DeferredEquityPlan {
+    address human_resources;
+
+    address payable employee; // bob
+    bool active = true; // this employee is active at the start of the contract
+
+    // @TODO: Set the total shares and annual distribution
+    uint total_shares = 1000;
+    uint annual_distribution = 250; // 1000 shares for 4 years
+
+    uint fakenow = now;
+    uint start_time = now; // permanently store the time this contract was initialized
+
+    // @TODO: Set the `unlock_time` to be 365 days from now
+    uint public unlock_time = start_time + 365;
+
+    uint public distributed_shares; // starts at 0
+
+    constructor(address payable _employee) public {
+        human_resources = msg.sender;
+        employee = _employee;
+    }
+
+    function distribute() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
+        require(active == true, "Contract not active.");
+
+        // @TODO: Add "require" statements to enforce that:
+        // 1: `unlock_time` is less than or equal to `now`
+        require(unlock_time <=now, "Shares are not yet vested");
+        // 2: `distributed_shares` is less than the `total_shares`
+        require(distributed_shares <= total_shares, "All Shares distributed");
+        
+
+        // @TODO: Add 365 days to the `unlock_time`
+        unlock_time +=365;
+
+        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
+        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
+        distributed_shares = ((now - start_time) / 365 days * annual_distribution);
+
+        // double check in case the employee does not cash out until after 5+ years
+        if (distributed_shares > 1000) {
+            distributed_shares = 1000;
+        }
+    }
+
+    // human_resources and the employee can deactivate this contract at-will
+    function deactivate() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        active = false;
+    }
+
+    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
+    function() external payable {
+        revert("Do not send Ether to this contract!");
+    }
+    function fastforward() public {
+    fakenow += 100;
+}
+}
